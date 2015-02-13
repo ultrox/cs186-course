b1152f56f2de4f5f9c00cd74a3c1569d1b6301fa
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-02-13 01:46:42 -0800

    Fixing val to var

--- hw2/sql/core/src/main/scala/org/apache/spark/sql/execution/basicOperators.scala
+++ hw2/sql/core/src/main/scala/org/apache/spark/sql/execution/basicOperators.scala
@@ -101,8 +101,8 @@ case class PartitionProject(projectList: Seq[Expression], child: SparkPlan) exte
     val diskPartitions: DiskHashedRelation = DiskHashedRelation(input, keyGenerator)
     val diskIter = diskPartitions.getIterator()
     val generator: (Iterator[Row] => Iterator[Row]) = CS186Utils.generateCachingIterator(projectList, child.output)
-    val partition: DiskPartition = null;
-    val rowIter :Iterator[Row]= null;
+    var partition: DiskPartition = new DiskPartition("")
+    var rowIter :Iterator[Row]= new Row()
 
     new Iterator[Row] {
       def hasNext() = {
