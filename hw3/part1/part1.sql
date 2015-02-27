dd1a8fd2dea0bf78e1f0388679a8d478d13b2a02
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-02-27 05:22:48 -0800

    Here's to part 1...now debugging for part 1 begins

--- hw3/part1/part1.sql
+++ hw3/part1/part1.sql
@@ -38,32 +38,89 @@ AS
 -- Question 2
 CREATE VIEW q2(from_name, to_name)
 AS
-  SELECT 1,1 -- replace this line
+  SELECT B.name, C.name
+  FROM intercommittee_transactions AS A
+  INNER JOIN committee_contributions AS B
+    ON B.cmte_id = A.cmte_id
+    AND B.pty_affiliation = DEM
+  INNER JOIN committee_contributions AS C
+    ON C.cmte_id = A.other_id
+    AND C.pty_affiliation = 'DEM'
 ;
 
 -- Question 3
 CREATE VIEW q3(name)
 AS
-  SELECT 1 -- replace this line
+  SELECT DISTINCT C.name
+  FROM  committee_contributions AS C
+  WHERE C.cmte_id NOT IN 
+    (SELECT A.cmte_id
+     FROM intercommittee_transactions AS A
+     INNER JOIN committee_contributions AS B
+       ON A.other_id = B.cmte_id
+       AND B.name = 'OBAMA, BARACK')-- replace this line
 ;
 
 -- Question 4.
 CREATE VIEW q4 (name)
 AS
-  SELECT 1 -- replace this line
+  WITH cand_com_count(cand_id, cmte_count) AS (
+    SELECT A.cand_id, COUNT(DISTINCT A.cmte_id)
+    FROM committee_contributions AS A
+    GROUP BY A.cand_id)
+  SELECT cand.name
+  FROM candidates AS cand, cand_com_count AS cand_com
+  WHERE cand.id = cand_com.cand_id
+     AND cand_com.cmte_count > (SELECT COUNT(DISTINCT cmte_tbl.cmte_id) * 0.01 
+                                FROM committees AS cmte_tbl) -- replace this line
 ;
 
 -- Question 5
 CREATE VIEW q5 (name, total_pac_donations) AS
-  SELECT 1,1 -- replace this line
+  WITH  full_comms(id, name) AS ( SELECT A.id, A.name
+                        FROM committees AS A
+                        GROUP BY A.id )
+        indiv_cont(id, amt) AS ( SELECT B.cmte_id, SUM(B.transaction_amt)
+                        FROM individual_contributions AS B
+                        WHERE B.entity_tp = 'ORG'
+                        GROUP BY B.cmte_id)
+  SELECT
+  FROM full_comms.name,indiv_cont.amt
+  LEFT OUTER JOIN indiv_cont 
+    ON full_comms.id = indiv_cont.id -- replace this line
 ;
 
 -- Question 6
 CREATE VIEW q6 (id) AS
-  SELECT 1 -- replace this line
+  WITH  comm1 AS ( SELECT A.cand_id
+                   FROM committee_contributions AS A
+                   WHERE A.cand_id IS NOT NULL
+                     AND A.entity_tp = 'PAC'
+                  GROUP BY A.cand_id )
+        comm2 AS ( SELECT B.cand_id
+                   FROM committee_contributions AS B
+                   WHERE B.cand_id IS NOT NULL
+                     AND B.entity_tp = 'CCM'
+                   GROUP BY B.cand_id)
+  SELECT DISTINCT cand_id
+  FROM comm1
+  INNER JOIN comm2
+    ON comm1.cand_id = comm2.cand_id
 ;
 
 -- Question 7
 CREATE VIEW q7 (cand_name1, cand_name2) AS
-  SELECT 1,1 -- replace this line
+    WITH (SELECT comm_cont.cmte_id, comm_cont.cand_id, cands.name
+          FROM committee_contributions AS comm_cont, candidates AS cands
+          WHERE comm_cont.state = 'RI'
+            AND cands.id  = comm_cont.cand_id) AS tbl1(cmte_id,cand_id,name)
+         (SELECT comm_cont.cmte_id, comm_cont.cand_id, cands.name
+          FROM committee_contributions AS comm_cont, candidates AS cands
+          WHERE comm_cont.state = 'RI'
+            AND cands.id  = comm_cont.cand_id) AS tbl2(cmte_id,cand_id,name)
+    SELECT tbl1.name,tbl2.name
+    FROM tbl1
+    INNER JOIN tbl2
+      ON  tbl1.cmte_id  = tbl2.cmte_id
+      AND tbl1.cand_id != tbl2.cand_id -- replace this line
 ;
