4bcdfaaa16a58aaa4032df6d83fd7df55583b889
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-02-27 20:06:42 -0800

    Debugging Part 2:

--- hw3/part2/maxflow.py
+++ hw3/part2/maxflow.py
@@ -145,10 +145,10 @@ def maxflow(bfs_max_iterations=float('inf'), flow_max_iterations=float('inf')):
                 WHERE E.id = FTR.edge_id
                 UNION
                 SELECT FE.reverse_id, E.capacity+FTR.flow
-                FROM edge E,(flow_to_route AS FTR
+                FROM edge E, flow_to_route AS FTR
                 INNER JOIN flip_edge AS FE
                   ON FE.reverse_id = E.id
-                  AND FTR.edge_id  = FE.forward_id)
+                  AND FTR.edge_id  = FE.forward_id
             )
             UPDATE edge
               SET capacity = updates.new_capacity
