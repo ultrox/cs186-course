9da3a7e0306ba8deae4fc7d4ba15843952a67246
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-04-07 18:20:37 -0700

    Pushing task 1 for testing

---
+++ hw5/myfecviz/services/fec.py
@@ -0,0 +1,53 @@
+from werkzeug.local import LocalProxy
+import ujson
+
+from myfecviz import get_db
+
+db = LocalProxy(get_db)
+
+
+def get_number_of_candidates():
+    """Return the number of candidates registered with the FEC.
+
+    This serves as just an example query.
+
+    :returns: integer
+    """
+    # Execute database query
+    db.execute("SELECT COUNT(*) FROM candidates;")
+    results = db.fetchall()
+
+    # Package into output
+    return int(results[0][0])
+
+
+def get_all_transaction_amounts():
+    """Return all transaction amounts with the state that the contribution came from.
+
+    For all committee contributions with a transaction_amt greater than zero,
+    return every transaction amount with the state that the contribution came form.
+
+    :return: List of dictionaries with 'state' and 'amount' keys
+    """
+    
+    # Execute database query
+    db.execute("SELECT state AS 'state', transaction_amt AS 'amount' FROM committee_contributions WHERE transaction_amt > 0")
+    results = db.fetchall()
+    # Package into output
+    return results
+
+
+def get_total_transaction_amounts_by_state():
+    """Return a list of dicts containing the state and total contributions.
+
+    For all committee contributions with a transaction_amt greater than zero,
+    return a dictionary containing the state and total amount.
+
+    :returns: List of dictionaries with 'state' and 'total_amount' keys
+    """
+    
+    # Execute database query
+    db.execute("SELECT state AS 'state', transaction_amt AS 'total_amount' FROM committee_contributions WHERE transaction_amt > 0 GROUP BY state")
+    results = db.fetchall()
+    # Package into list of dictionaries
+    return results
