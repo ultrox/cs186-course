65268ef36f4aef1dbc6a738f822de7d987cb8576
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-04-07 19:12:40 -0700

    added print statements to the tests

--- hw5/myfecviz/tests/test_fec_services.py
+++ hw5/myfecviz/tests/test_fec_services.py
@@ -14,13 +14,14 @@ class FECServiceTest(FlaskClientTestCase):
         results = fec.get_all_transaction_amounts()
 
         for result in results:
+            print(result)
             self.assertLess(0, result['amount'])
 
     def test_get_all_transaction_amounts(self):
         """Verify the first dict has the right keys."""
         results = fec.get_all_transaction_amounts()
         result = results[0]
-
+        print(result)
         # Inspect first result for proper keys
         self.assertIn('state', result)
         self.assertIn('amount', result)
@@ -28,7 +29,7 @@ class FECServiceTest(FlaskClientTestCase):
     def test_get_total_transaction_amounts_by_state(self):
         """Verify transaction amount groupings have the right keys."""
         results = fec.get_total_transaction_amounts_by_state()
-
+        print(result[0])
         result = results[0]
         self.assertIn('state', result)
         self.assertIn('total_amount', result)
