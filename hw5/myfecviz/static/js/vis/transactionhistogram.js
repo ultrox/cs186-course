f35ac0b458ed7b69241a0842fb804921c1393591
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-04-09 02:24:07 -0700

    maybe

--- hw5/myfecviz/static/js/vis/transactionhistogram.js
+++ hw5/myfecviz/static/js/vis/transactionhistogram.js
@@ -96,7 +96,7 @@ TransactionHistogram.prototype.render = function(data) {
         .attr("y", function(d) { 
             var temp = 2+that.height - that.yScale(d.y);
             if (temp>(that.height-10))
-                return that.height - that.yScale(d.y);
+                return that.height - that.yScale(d.y) - 10;
             return temp;})
         .attr("x", this.xScale(histogramData[0].dx) / 2)
         .attr("text-anchor", "middle")
