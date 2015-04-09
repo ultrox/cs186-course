d63a96b1ca4d347096221a385be865c32544bd62
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-04-09 01:43:28 -0700

    Maybe something is happening..?

--- hw5/myfecviz/static/js/vis/transactionhistogram.js
+++ hw5/myfecviz/static/js/vis/transactionhistogram.js
@@ -79,6 +79,7 @@ TransactionHistogram.prototype.render = function(data) {
     // Add a rectangle to this bar grouping
     bar.append("rect")
         .attr("x", 1 )
+        .attr("y", function(d) { return that.height - that.yScale(d.y);})
         .attr("width", this.width/this.bins.length)
         .attr("height", function(d) { return that.yScale(d.y); });
 
