b0860658b81efcaea60bc30503304317433c73da
Bryan Sieber <bryan_sieber@berkeley.edu>
2015-04-09 19:49:50 -0700

    trying scale instead of translate

--- hw5/myfecviz/static/js/vis/transactionhistogram.js
+++ hw5/myfecviz/static/js/vis/transactionhistogram.js
@@ -108,7 +108,7 @@ TransactionHistogram.prototype.render = function(data) {
         .data(histogramData, function(d) {return d.x;})
         .transition().duration(200)
         .attr("transform", function (d) {
-            return "scale( 0 , "+that.yScale(d.y)+" )";
+            return "scale( 1 , "+that.yScale(d.y)+" )";
         })
         .attr("height", function(d) { return that.yScale(d.y); });
 
